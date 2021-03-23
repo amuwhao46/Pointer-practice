5e7d97a4629eada97366a1cac8eaac708928b2de
Oke Amuwha <amuwhao46@gmail.com>
2021-03-23 15:42:27 -0400

    Added the rest of the data to be manipulated

--- Lab 12-1/main.cpp
+++ Lab 12-1/main.cpp
@@ -23,16 +23,28 @@ int main()
     // Declare variables
     double *baseRadius;
     double *height;
+    baseRadius = new double;
     
     // Set precision
     cout << fixed << showpoint << setprecision(2);
     
     // Formula
+    baseRadius = new double;
+    *baseRadius = 1.5;
+    height = new double;
+    *height = 2 * (*baseRadius);
+    baseRadius = new double;
+    *baseRadius = 4.0;
 
     // Show application header
     cout << "Welcome to my Application!" << endl;
     cout << "--------------------------" << endl << endl;
-
+    
+    cout << "Radius of the base: " << baseRadius << endl;
+    cout << "Height: " << height << endl;
+    
+    cout << "Volume: " << 3.14 * (baseRadius) * (baseRadius) << endl;
+    cout << "Surface area: " << 2 * 3.14 * (baseRadius) * (height) << endl;
 
     // Show application close
     cout << "\nEnd of my Application" << endl;
