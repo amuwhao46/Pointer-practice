f98d95ff84a4549baa13c465e42bebbd3f1644dc
Oke Amuwha <amuwhao46@gmail.com>
2021-03-23 15:22:14 -0400

    Initial Commit

---
+++ Lab 12-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 12-1
+//
+//  Created by Oke-Oghene Amuwha on 3/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
